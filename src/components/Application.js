ad632d13b25051c6d88e62c89078916d03ab7adf
TristanCole <tristanleighcole@googlemail.com>
2022-01-22 12:03:19 -0700

    Retrieving appointments multiple promise resolves.

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,63 +3,95 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import Axios from "axios";
+import { getAppointmentsForDay } from "helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  {
-    id: 5,
-    time: "4pm",
-  },
-];
+// const appointments = [
+  // {
+  //   id: 1,
+  //   time: "12pm",
+  // },
+  // {
+  //   id: 2,
+  //   time: "1pm",
+  //   interview: {
+  //     student: "Lydia Miller-Jones",
+  //     interviewer: {
+  //       id: 3,
+  //       name: "Sylvia Palmer",
+  //       avatar: "https://i.imgur.com/LpaY82x.png",
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 3,
+  //   time: "2pm",
+  // },
+  // {
+  //   id: 4,
+  //   time: "3pm",
+  //   interview: {
+  //     student: "Archie Andrews",
+  //     interviewer: {
+  //       id: 4,
+  //       name: "Cohana Roy",
+  //       avatar: "https://i.imgur.com/FK8V841.jpg",
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 5,
+  //   time: "4pm",
+  // },
+// ];
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    //appointments: {}
+    appointments: {}
   });
+
+  // const dailyAppointments = [{
+  //   id: 1,
+  //   time: "12pm",
+  // },
+  // {
+  //   id: 2,
+  //   time: "1pm",
+  //   interview: {
+  //     student: "Lydia Miller-Jones",
+  //     interviewer: {
+  //       id: 3,
+  //       name: "Sylvia Palmer",
+  //       avatar: "https://i.imgur.com/LpaY82x.png",
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 3,
+  //   time: "2pm",
+  // },
+  // {
+  //   id: 4,
+  //   time: "3pm",
+  //   interview: {
+  //     student: "Archie Andrews",
+  //     interviewer: {
+  //       id: 4,
+  //       name: "Cohana Roy",
+  //       avatar: "https://i.imgur.com/FK8V841.jpg",
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 5,
+  //   time: "4pm",
+  // },];
 
   const setDay = (day) => setState({ ...state, day }); // added this as part of the refactor
 
-  const setDays = (days) => {
-    // added this as part of the refactor
-    //... your code here ...
-    setState((prev) => ({ ...prev, days }));
-  };
-
-  const showAppointments = appointments.map((appointment) => (
+  const showAppointments = getAppointmentsForDay(state, state.day).map((appointment) => ( // getting the appointments for each day specified
+    // alternate code for below Appointment = <Appointment key={appointment.id} {...appointment} />
     <Appointment
       key={appointment.id}
       id={appointment.id}
@@ -67,12 +99,22 @@ export default function Application(props) {
       interview={appointment.interview}
     />
   ));
-  // alternate code = <Appointment key={appointment.id} {...appointment} />
 
+  // Intital request to get all the data from the three endpoints, days, appointments, interviewers
   useEffect(() => {
-    const testURL = `http://localhost:8001/api/days`;
-    Axios.get(testURL).then((response) => {
-      setDays([...response.data]);
+    const fetchDays = Axios.get('http://localhost:8001/api/days');
+    const fetchAppointments = Axios.get('http://localhost:8001/api/appointments');
+    const fetchInterviewers = Axios.get('http://localhost:8001/api/interviewers');
+    
+    Promise.all([      
+      Promise.resolve(fetchDays),
+      Promise.resolve(fetchAppointments),
+      Promise.resolve(fetchInterviewers)
+
+    ]).then((response) => {
+      setState(prev => ({...prev, days: response[0].data, appointments: response[1].data, interviewers: response[2].data }));
+    }).catch(err => {
+      console.log(err);
     });
   }, []);
 
